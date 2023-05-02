42a44a5fa63f3362e96331101b525b709a6aa7e7
IngridAnne <ingridanne3009@gmail.com>
2023-05-02 13:27:12 +0200

    added some comments

--- fresh_student/script/kalkulator.js
+++ fresh_student/script/kalkulator.js
@@ -1,9 +1,14 @@
-/* let displayEl = document.getElementById('#display') */
+//Legger til en lytter
 document.addEventListener("keydown", calculate)
+
+//funksjonen som kjøres hvis en key presses ned
 function calculate(e){
+
+    //forhindrer en default
     e.preventDefault()
+
+    //sjekker om keyen som er presset == enter
     if (e.keyCode == 13){
         display.value = eval(display.value)
-
     }
 }
